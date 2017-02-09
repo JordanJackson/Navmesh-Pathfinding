01066e360987c322a0514cf807c8691bcc867e66
Jordan Jackson <jordanpatrickjackson@gmail.com>
2017-02-08 22:12:55 -0500

    Added modular unit selection
    Unit selection and UI refactored to be more modular.

--- Assets/OverlordController.cs
+++ Assets/OverlordController.cs
@@ -7,7 +7,7 @@ public class OverlordController : MonoBehaviour
 {
     public Button[] unitButtons;
     public int defaultSelection = 0;
-    int currentSelection;
+    public int currentSelection;
 
     private void Start()
     {
@@ -16,14 +16,32 @@ public class OverlordController : MonoBehaviour
 
     private void Update()
     {
-        unitButtons[currentSelection].GetComponent<Image>().color = Color.green;
+        
+    }
 
+    void OnGUI()
+    {
         Event e = Event.current;
 
-        if (Input.anyKeyDown && e.isKey)
+        if (e.isKey)
         {
-            
+            // convert Alpha# to int
+            int keyToID = (int)e.keyCode - 49;
+            // if valid unit selection
+            if (keyToID >= 0 && keyToID < unitButtons.Length)
+            {
+                currentSelection = keyToID;
+            }
         }
+
+        for (int i = 0; i < unitButtons.Length; i++)
+        {
+            if (i != currentSelection)
+            {
+                unitButtons[i].GetComponent<Image>().color = Color.grey;
+            }
+        }
+        unitButtons[currentSelection].GetComponent<Image>().color = Color.white;
     }
 
      
